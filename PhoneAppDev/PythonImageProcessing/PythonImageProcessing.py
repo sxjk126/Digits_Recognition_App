8a7e1c591483023a89910dd35c2f0460b478ae03
Xin Shan <xin.shan_88@outlook.com>
2019-11-15 16:02:28 -0800

    Rewrite ImageProcessing using Python

--- PhoneAppDev/PythonImageProcessing/PythonImageProcessing.py
+++ PhoneAppDev/PythonImageProcessing/PythonImageProcessing.py
@@ -1,4 +1,4 @@
-from skimage.transform import rescale
+#from skimage.transform import rescale
 from skimage.transform import resize
 from skimage import data, io
 from matplotlib import pyplot as plt
@@ -6,14 +6,27 @@ from PIL import Image
 from numpy import asarray
 import codecs, json 
 import numpy as np
-image_gray = io.imread(r'D:\Repos\Number_Two.jpg', as_gray=True)
-img_resized = resize(image_gray, (28,28))
-data = asarray(img_resized)
-b = np.array(data)
-a = b.flatten()
-c = a.tolist()
 
-file_path = r"D:\Repos\NumTwo.json" 
-#### this saves the array in .json format
-json.dump(c, codecs.open(file_path, 'w', encoding='utf-8'), 
-          separators=(',', ':'), sort_keys=True, indent=4) 
+class ImageProcessing:
+    def ImageConverter(fromFile, toFile, width = 28, height = 28):
+        #Convert Original image to grayscale
+        image_gray = io.imread(fromFile, as_gray=True)
+
+        img_resized = resize(image_gray, (width, height))
+        # convert image to numpy array
+        data = asarray(img_resized)
+        #Convert data to list
+        dataList = data.tolist()
+        #file path for json file
+        file_path = toFile 
+        # this saves the data in .json format
+        json.dump(c, codecs.open(file_path, 'w', encoding='utf-8'), 
+                separators=(',', ':'), sort_keys=True, indent=4) 
+
+    def PlotImage(oriImage, proImage):
+        #plot images
+        plt.subplot(121), io.imshow(oriImage)
+        plt.title('Original Image')
+        plt.subplot(122), io.imshow(proImage)
+        plt.title('Processed Image')
+        plt.show()
